e586812195161c4a9f4bb294917775368e1b4358
zhoulei0232 <5971794@qq.com>
2020-09-17 11:41:06 +0800

    er commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Layout, Menu  } from 'antd';
 import './App.css';
+import './App.less';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="my-layout">
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+        <Menu.Item key="4" icon={<UserOutlined />}>
+          nav 4
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout>
+      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0' }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
   );
 }
 
