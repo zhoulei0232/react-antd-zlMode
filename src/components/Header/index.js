227df8a184c3db197252a80166b779d384c2c5cc
zhoulei0232 <5971794@qq.com>
2020-09-23 17:49:33 +0800

    2020-09-233

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import {Row , Col} from 'antd'
 import './index.less'
+import Util from '../../utils/utils'
 
 class Header extends React.Component{
+    componentWillMount(){
+        this.setState({
+            userName:'管理员',
+            sysTime:''
+        })
+        setInterval(()=>{
+           let sysTime = Util.formateDate(new Date().getTime())
+            this.setState({
+                sysTime
+            })
+        },1000)
+    }
     render(){
         return(
-            <div  className="header">
-                 我是头部
+            <div className="header">
+                <Row className="header-top">
+                    <Col  span="24">
+                        <span>{this.state.userName}</span>
+                        <a href="#">退出</a>
+                    </Col>
+                    
+                </Row>
+                <Row className="breadcrumb" >
+                    <Col  span="4" className="breadcrumb-title" >
+                        首页
+                    </Col>
+                    <Col  span="20" className="weather">
+                        <span className="date" >{this.state.sysTime}</span>
+                        <span className="weather-detail">多云</span>
+                    </Col>
+                </Row>
             </div>
         )
     }
--- src/pages/ui/buttons.js
+++ src/pages/ui/buttons.js
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import {Card,Button,Icon}  from  'antd'
+import { PlusOutlined,EditOutlined,DeleteOutlined,SearchOutlined ,DownloadOutlined} from '@ant-design/icons';
+import './ui.less'
 class  Buttons  extends  React.Component{
 
     render(){
         return(
-            <div>啊啊啊啊</div>
+            <div className="content-details">
+                <Card  title="基础按钮">
+                    <Button type="primary">Imooc</Button>
+                    <Button  >Imooc</Button>
+                    <Button type="dashed" >Imooc</Button>
+                    <Button type="danger" >Imooc</Button>
+                    <Button disabled >Imooc</Button>
+                </Card>
+                <Card  title="基础按钮">
+                    <Button icon={<PlusOutlined />}>创建</Button>
+                    <Button icon={<EditOutlined />}>编辑</Button>
+                    <Button icon={<DeleteOutlined />} >删除</Button>
+                    <Button shape="circle" 
+                            icon={<SearchOutlined />}  >
+                    </Button>
+                    <Button type="primary" icon={<SearchOutlined />}  >搜索</Button>
+                    <Button type="primary" icon={<DownloadOutlined />}   >下载</Button>
+                </Card>
+            </div>
         )
     }
 }
