499055858c2b428b14144d7ed64ff2e7235d2257
zhoulei0232 <5971794@qq.com>
2020-09-25 09:31:48 +0800

    20200925早上

--- src/pages/ui/buttons.js
+++ src/pages/ui/buttons.js
@@ -1,20 +1,46 @@
 import React from 'react'
-import {Card,Button,Icon}  from  'antd'
-import { PlusOutlined,EditOutlined,DeleteOutlined,SearchOutlined ,DownloadOutlined} from '@ant-design/icons';
+import {Card,Button,Radio}  from  'antd'
+import { PlusOutlined,EditOutlined,DeleteOutlined,SearchOutlined ,DownloadOutlined ,LeftOutlined , RightOutlined} from '@ant-design/icons';
 import './ui.less'
+ 
 class  Buttons  extends  React.Component{
-
+    constructor(props){
+        super(props);
+        this.state={
+            loading:true,
+            size:'default'
+        }
+    }
+    //关闭loading
+    handleCloseLoading=()=>{
+        this.setState({
+            loading:false
+        })
+    }
+    //打开loading 
+    handleOpenLoading=()=>{
+        this.setState({
+            loading:true
+        })
+    }
+    //改变大小
+    handleChangeSize=(e)=>{
+        let size = e.target.value ;
+        this.setState({
+            size
+        })
+    }
     render(){
         return(
             <div className="content-details">
-                <Card  title="基础按钮">
+                <Card  title="基础按钮"  className="card-wrap">
                     <Button type="primary">Imooc</Button>
                     <Button  >Imooc</Button>
                     <Button type="dashed" >Imooc</Button>
                     <Button type="danger" >Imooc</Button>
                     <Button disabled >Imooc</Button>
                 </Card>
-                <Card  title="基础按钮">
+                <Card  title="图形按钮"  className="card-wrap">
                     <Button icon={<PlusOutlined />}>创建</Button>
                     <Button icon={<EditOutlined />}>编辑</Button>
                     <Button icon={<DeleteOutlined />} >删除</Button>
@@ -23,6 +49,32 @@ class  Buttons  extends  React.Component{
                     </Button>
                     <Button type="primary" icon={<SearchOutlined />}  >搜索</Button>
                     <Button type="primary" icon={<DownloadOutlined />}   >下载</Button>
+                </Card>
+                <Card  title="Loading按钮"  className="card-wrap">
+                    <Button type="primary"  loading={this.state.loading}  onClick={this.handleOpenLoading}>确定</Button>
+                    <Button type="primary" shape="circle"  loading={this.state.loading}  ></Button>
+                    <Button loading = {this.state.loading}>点击加载</Button>
+                    <Button shape="circle" 
+                            loading={this.state.loading}  >
+                    </Button>
+                    <Button type="primary"  onClick={this.handleCloseLoading} >关闭</Button>
+                </Card>
+                <Card  title="按钮组"  className="card-wrap">
+                    <Button.Group>
+                        <Button type="primary" icon={<LeftOutlined />}></Button>
+                        <Button type="primary" icon={<RightOutlined />}></Button>
+                    </Button.Group>
+                </Card>
+                <Card  title="按钮尺寸"  className="card-wrap">
+                    <Radio.Group value={this.state.size}  onChange={this.handleChangeSize}>
+                          <Radio value="small">小</Radio>
+                          <Radio value="default">中</Radio>
+                          <Radio value="large">大</Radio>
+                    </Radio.Group>
+                    <Button  type="primary" size={this.state.size}>按钮</Button>
+                    <Button  size={this.state.size}>按钮</Button>
+                    <Button  type="dashed" size={this.state.size}>按钮</Button>
+                    <Button  type="danger" size={this.state.size}>按钮</Button>
                 </Card>
             </div>
         )
--- src/pages/ui/modals.js
+++ src/pages/ui/modals.js
@@ -1,10 +1,102 @@
 import React from 'react'
+import {Card , Button ,Modal} from  'antd'
+import './ui.less'
 
 class  Modals  extends  React.Component{
-
+    constructor(props){
+        super(props)
+        this.state = {
+            showModal1 : false ,
+            showModal2 : false,
+            showModal3 : false,
+            showModal4 : false
+        }
+    }
+    //打开模态框
+    handleOpen = (type)=>{
+        console.log(123,type)
+        this.setState({
+            [type]:true
+        })
+    }
+    handleConfirm = (type)=>{
+        Modal[type]({
+            title:'确认？',
+            content:'今天天气怎么样？',
+            okText:"fine",
+            cancelText:"见鬼",
+            onOk(){
+                console.log('fine')
+            },
+            onCancel(){
+                console.log('不好')
+            }
+        })
+    }
     render(){
         return(
-            <div>Modals</div>
+            <div  className="content-details">
+                <Card title="基础模态框"  className="card-wrap" >
+                    <Button type="primary" onClick={() => this.handleOpen('showModal1')}>Open</Button>
+                    <Button type="primary" onClick={() => this.handleOpen('showModal2')} >自定义页脚</Button>
+                    <Button type="primary" onClick={() => this.handleOpen('showModal3')} >顶部20px弹框</Button>
+                    <Button type="primary" onClick={() => this.handleOpen('showModal4')} >水平居中</Button>
+                </Card>
+                <Card title="信息确认框"  className="card-wrap" >
+                    <Button type="primary"  onClick={()=>this.handleConfirm('confirm')}>Confirm</Button>
+                    <Button type="primary"  onClick={()=>this.handleConfirm('info')}>info</Button>
+                    <Button type="primary"  onClick={()=>this.handleConfirm('success')}>success</Button>
+                    <Button type="primary"  onClick={()=>this.handleConfirm('warning')}>warning</Button>
+                </Card>
+                <Modal
+                    title="React"
+                    visible={this.state.showModal1}
+                    onCancel={()=>(
+                        this.setState({
+                            showModal1:false
+                        })
+                    )}
+                >
+                    <p>欢迎使用模态框</p>
+                </Modal>
+                <Modal
+                    title="React"
+                    visible={this.state.showModal2}
+                    okText="好的"
+                    cancelText="取消"
+                    onCancel={()=>(
+                        this.setState({
+                            showModal2:false
+                        })
+                    )}
+                >
+                    <p>欢迎使用模态框</p>
+                </Modal>
+                <Modal
+                    title="React"
+                    style={{top:20}}
+                    visible={this.state.showModal3}
+                    onCancel={()=>(
+                        this.setState({
+                            showModal3:false
+                        })
+                    )}
+                >
+                    <p>欢迎使用模态框</p>
+                </Modal>
+                <Modal
+                    title="React"
+                    wrapClassName="vertical-center-modal"
+                    visible={this.state.showModal4}
+                    onCancel={()=>(
+                        this.setState({
+                            showModal4:false
+                        })
+                    )}
+                >
+                    <p>欢迎使用模态框</p>
+                </Modal>
+            </div>
         )
     }
 }
